f6c93ebc0f79b93ba907b5089595f03e902ffdc1
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-24 09:28:29 -0400

    set up typedefs and resolvers for apollo

---
+++ src/server/schemas/resolvers.js
@@ -0,0 +1,32 @@
+const { Patient, User, Disease, Fact } = require('../models');
+
+const resolvers = {
+    Query: {
+        onePatient: async (parent, args) => {
+            return await Patient.findOne({ guardianEmail: args.email})
+        },
+        oneUser: async (parent, args) => {
+            return await User.findOne({ email: args.email})
+        },
+        diseases: async () => {
+            return await Disease.find({})
+        },
+        facts: async () => {
+            return await Fact.find({})
+        }
+    },
+    Mutation: {
+        addUser: async(parent, { firstName, lastName, provider, email, password}) => {
+            return await User.create({firstName, lastName, provider, email, password})
+        },
+        addMessage: async (parent, { email, to, date, time, content}) => {
+            return await Patient.findOneAndUpdate(
+                { guardianEmail: email },
+                { $addToSet: { messages: { to, date, time, content} }},
+                { runValidators: true , new: true}
+                )
+        }
+    }
+}
+
+module.exports = resolvers;
