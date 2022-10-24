7fa4a66a925d6b90df3fef5c486f2c8ff083dac4
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-24 19:16:45 -0400

    added jeopardy questions to grid but need to make it a functional game

--- client/src/pages/Jeopardy.js
+++ client/src/pages/Jeopardy.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import data from '../utils/jeopardyData';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 //TODO: change into having a card rendered for each question that is styled and has onclick funcionality but for now using this to see how to correctly reference the data
 const Jeopardy = () => {
+
     const Item = styled(Paper)(({ theme }) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
         ...theme.typography.body2,
@@ -14,46 +18,58 @@ const Jeopardy = () => {
         textAlign: 'center',
         color: theme.palette.text.secondary,
       }));
+
     return(
-        // <div>
-        //     {data.map((category) => (
-        //         <div>
-        //             <h1>{category.category}</h1>
-        //             <p>{category.questions[0].points}</p>
-        //             <p>{category.questions[0].question}</p>
-        //             <p>{category.questions[0].answer}</p>
-
-        //             <p>{category.questions[1].points}</p>
-        //             <p>{category.questions[1].question}</p>
-        //             <p>{category.questions[1].answer}</p>
-
-        //             <p>{category.questions[2].points}</p>
-        //             <p>{category.questions[2].question}</p>
-        //             <p>{category.questions[2].answer}</p>
-
-        //             <p>{category.questions[3].points}</p>
-        //             <p>{category.questions[3].question}</p>
-        //             <p>{category.questions[3].answer}</p>
-        //         </div>
-        //     ))}
-            
-        // </div>
-        <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
       {data.map((category) => (
         <Grid item xs={3}>
-        <Item>{category.category}</Item>
+        <Item sx={{background: 'red'}}>{category.category}</Item>
+        <Item>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {`${category.questions[0].points} points` }
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                {category.questions[0].question}
+                </Typography>
+            </CardContent>
+        </Item>
+
+        <Item>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {`${category.questions[1].points} points` }
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                {category.questions[1].question}
+                </Typography>
+            </CardContent>
+        </Item>
+
+        <Item>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {`${category.questions[2].points} points` }
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                {category.questions[2].question}
+                </Typography>
+            </CardContent>
+        </Item>
+
+        <Item>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {`${category.questions[3].points} points` }
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                {category.questions[3].question}
+                </Typography>
+            </CardContent>
+        </Item>
       </Grid>
                 ))}
-        {/* <Grid item xs={3}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>xs=8</Item>
-        </Grid> */}
       </Grid>
     </Box>
     )
