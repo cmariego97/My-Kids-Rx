01bc1e74ecb19a919a5f775babf142cacdf1a60e
cmarie.go97 <110327167+cmariego97@users.noreply.github.com>
2022-10-26 14:57:57 -0400

    added mongo string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/schools-db',
+  process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.klxefmh.mongodb.net/?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
