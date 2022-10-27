be4874673ae31a4a2c2526f70019d8eb7cfd040b
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-27 10:12:37 -0400

    added login and create acct mutations to client side as well as jwt decode

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,6 +11,27 @@ export const ADD_USER = gql`
     }
   `
 
+//change to this when ready to implement login
+// mutation addProfile($firstName: String!, $lastName: String!, $provider: String!, $email: String!, $password: String!) {
+//   addProfile(firstName: $firstName, lastName: $lastName, provider: $provider, email: $email, password: $password) {
+//     token
+//     profile {
+//       email
+//     }
+//   }
+// }
+
+// export const LOGIN_USER = gql`
+//   mutation login($email: String!, $password: String!) {
+//     login(email: $email, password: $password) {
+//       token
+//       profile {
+//         email
+//       }
+//     }
+//   }
+// `;
+
 //add new message
 export const ADD_MESSAGE = gql`
     mutation ($email: String!, $to: String!, $date: String!, $time: String!, $content: String!) {
