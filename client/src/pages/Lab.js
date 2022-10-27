276f5755f5aa981570ee05f5b435e18cf4ca7462
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-27 13:15:38 -0400

    added conditional rendering of labs to account for if a pt has no lab results on file

--- client/src/pages/Lab.js
+++ client/src/pages/Lab.js
@@ -16,76 +16,86 @@ const Lab = () => {
     }
     else {
         console.log(data);
-        const lab = data.onePatient.labs[0]
-        return (
-            <div>
-                <p>{`Showing Results from: ${lab.date}`}</p>
-                <p>Complete Blood Count</p>
-                <li>
-                    {`RBC: ${lab.cbc.rbc} cells/mcL`}
-                </li>
-                <li>
-                    {`WBC: ${lab.cbc.wbc} cells/mcL`}
-                </li>
-                <li>
-                    {`Platelets: ${lab.cbc.platelets} cells/mcL`}
-                </li>  
-                <li>
-                    {`Hematocrit: ${lab.cbc.hematocrit} percent`}
-                </li>
-                <p>Comprehensive Metabolic Panel</p>
-                <li>
-                    {`Sodium: ${lab.cmp.sodium} mEq/L`}
-                </li>
-                <li>
-                    {`potassium: ${lab.cmp.potassium} mEq/L`}
-                </li>
-                <li>
-                   {` chloride: ${lab.cmp.chloride} mmol/L`}
-                </li>
-                <li>
-                    {`calcium: ${lab.cmp.calcium} mg/dL`}
-                </li>
-                <li>
-                    {`glucose: ${lab.cmp.glucose} mg/dL`}
-                </li>
-                <li>
-                    {`carbon dioxide: ${lab.cmp.carbonDioxide} mmol/L`}
-                </li>
-                <li>
-                   {` bun: ${lab.cmp.bun} mg/dL`}
-                </li>
-                <li>
-                    {`creatinine: ${lab.cmp.creatinine} mg/dL`}
-                </li>
-                <li>
-                    {`alp: ${lab.cmp.alp} IU/L`}
-                </li>
-                <li>
-                    {`alt: ${lab.cmp.alt} IU/L`}
-                </li>
-                <li>
-                    {`ast: ${lab.cmp.ast} IU/L`}
-                </li>
-                <li>
-                    {`bilirubin: ${lab.cmp.bilirubin} mg/dL`}
-                </li>
-                <li>
-                    {`albumin: ${lab.cmp.albumin} g/dL`}
-                </li>
-                <li>
-                    {`total protein: ${lab.cmp.totalProtein} g/dL`}
-                </li>
-                <p>Lipid Panel</p>
-                <li>{`Total Cholesterol: ${lab.lipid.total} mg/dL`}</li>
-                <li>{`LDL Cholesterol: ${lab.lipid.ldl} mg/dL`}</li>
-                <li>{`HDL Cholesterol: ${lab.lipid.hdl} mg/dL`}</li>
-                <li>{`Triglycerides: ${lab.lipid.tg} mg/dL`}</li>
-                <p>{`Hemoglobin: ${lab.hb} g/dL`}</p>
-                
-                
-            </div>
-        )
+        // const lab = data.onePatient.labs[0]
+        const labs = data.onePatient.labs;
+        if (labs.length === 0) {
+            return (
+                <p>No lab results on file!</p>
+            )
+        }
+        else {
+            return (
+                labs.map((lab) => (
+                    <div>
+                    <p>{`Results from ${lab.date}`}</p>
+                    <p>Complete Blood Count</p>
+                    <li>
+                        {`RBC: ${lab.cbc.rbc} cells/mcL`}
+                    </li>
+                    <li>
+                        {`WBC: ${lab.cbc.wbc} cells/mcL`}
+                    </li>
+                    <li>
+                        {`Platelets: ${lab.cbc.platelets} cells/mcL`}
+                    </li>  
+                    <li>
+                        {`Hematocrit: ${lab.cbc.hematocrit} percent`}
+                    </li>
+                    <p>Comprehensive Metabolic Panel</p>
+                    <li>
+                        {`Sodium: ${lab.cmp.sodium} mEq/L`}
+                    </li>
+                    <li>
+                        {`Potassium: ${lab.cmp.potassium} mEq/L`}
+                    </li>
+                    <li>
+                       {` Chloride: ${lab.cmp.chloride} mmol/L`}
+                    </li>
+                    <li>
+                        {`Calcium: ${lab.cmp.calcium} mg/dL`}
+                    </li>
+                    <li>
+                        {`Glucose: ${lab.cmp.glucose} mg/dL`}
+                    </li>
+                    <li>
+                        {`Carbon dioxide: ${lab.cmp.carbonDioxide} mmol/L`}
+                    </li>
+                    <li>
+                       {`BUN: ${lab.cmp.bun} mg/dL`}
+                    </li>
+                    <li>
+                        {`Creatinine: ${lab.cmp.creatinine} mg/dL`}
+                    </li>
+                    <li>
+                        {`ALP: ${lab.cmp.alp} IU/L`}
+                    </li>
+                    <li>
+                        {`ALT: ${lab.cmp.alt} IU/L`}
+                    </li>
+                    <li>
+                        {`AST: ${lab.cmp.ast} IU/L`}
+                    </li>
+                    <li>
+                        {`Bilirubin: ${lab.cmp.bilirubin} mg/dL`}
+                    </li>
+                    <li>
+                        {`Albumin: ${lab.cmp.albumin} g/dL`}
+                    </li>
+                    <li>
+                        {`Total protein: ${lab.cmp.totalProtein} g/dL`}
+                    </li>
+                    <p>Lipid Panel</p>
+                    <li>{`Total cholesterol: ${lab.lipid.total} mg/dL`}</li>
+                    <li>{`LDL cholesterol: ${lab.lipid.ldl} mg/dL`}</li>
+                    <li>{`HDL cholesterol: ${lab.lipid.hdl} mg/dL`}</li>
+                    <li>{`Triglycerides: ${lab.lipid.tg} mg/dL`}</li>
+                    <p>{`Hemoglobin: ${lab.hb} g/dL`}</p>
+                    
+                    
+                </div>
+                ))
+            )
+        }
     }
 }
 
