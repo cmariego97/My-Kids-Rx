b14d2156c1ae1f10664096ced959c30c01958b34
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-29 15:14:25 -0400

    adjusted layout of operation game to be responsive

--- client/src/pages/Operation.js
+++ client/src/pages/Operation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '@mui/material/Container';
+import '../assets/css/operation.css';
 //import game images
 import Man from '../assets/images/operation.jpeg';
 import Pencil from '../assets/images/pencil.png';
@@ -49,11 +50,15 @@ const Operation = ({changeGame}) => {
         document.querySelector('#score').textContent = score;
         document.querySelector('#warning').textContent = 'Oh no! You touched the patient or table! -1 point!'
       }
+    //   console.log(window.screen.width);
+    //   if (window.screen.width > 800) {
+    //     console.log(true)
+    //   }
     return (
-        <Container maxWidth="lg" sx={{display: 'flex'}}>
+        <Container id='cont' maxWidth="lg">
             {/* column 1 */}
-        <div style={{ background: 'white', height: '100vh', width: '50%'}}>
-                <img src={Man} alt='operation-game' style={{height: '100vh', border: '1px solid black'}} onClick={penalty}>
+        <div id='col-1' style={{ background: 'white'}}>
+                <img id='man' src={Man} alt='operation-game' style={{border: '1px solid black'}} onClick={penalty}>
                 </img>
                 <img src={Pencil} alt='pencil' style={{width: '100px', position: 'absolute', bottom: '510px', left: '190px'}} draggable="true" onDragStart={drag}
                 id="drag1"></img>
@@ -69,9 +74,9 @@ const Operation = ({changeGame}) => {
                 id="drag6"></img>
          </div>
          {/* column 2 */}
-         <div style={{width: '50%', display: 'flex', flexDirection: 'column'}}>
+         <div id='col-2' style={{display: 'flex'}}>
             <div 
-            style={{background: 'white', height: '50vh'}}
+            id='info' style={{background: 'white'}}
             >
                 <h2>Score: <span id='score'>0</span></h2>
                 <p id='warning'>Remove the objects that do not belong! Do not touch the patient or table!</p>
@@ -81,7 +86,7 @@ const Operation = ({changeGame}) => {
                 <p style={{marginTop: '30%'}}>Throw away objects in the container below</p>
             </div>
             <div 
-                style={{background: 'black', height: '50vh', borderRadius: '10px', border: '10px solid grey'}}
+                style={{background: 'black',borderRadius: '10px', border: '10px solid grey'}}
                 onDrop={drop}
                 onDragOver={allowDrop}
                 id="target"
