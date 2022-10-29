e2024dcdf7e506cd4ecc995eade22939b9d030f8
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-28 21:18:20 -0400

    added score counter and responsive message to operation

--- client/src/pages/Operation.js
+++ client/src/pages/Operation.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 //import images
 import Man from '../assets/images/operation.jpeg';
 
 const Operation = ({changeGame}) => {
+    let score = 0;
+    
     function drag(ev) {
         ev.dataTransfer.setData("text", ev.target.id);
       }
@@ -13,19 +15,27 @@ const Operation = ({changeGame}) => {
         ev.preventDefault();
       }
       //TODO: just drag to trash can icon or something
+      //TODO: add timer
       function drop(ev) {
         ev.preventDefault();
         var data = ev.dataTransfer.getData("text");
         ev.target.appendChild(document.getElementById(data));
-        console.log('dropped');
+        score++;
+        document.querySelector('#score').textContent = score;
+        document.querySelector('#warning').textContent = 'Object successfully removed! +1 point!'
       }
 
       function penalty() {
-        console.log(true);
+        score--;
+        document.querySelector('#score').textContent = score;
+        document.querySelector('#warning').textContent = 'Oh no! You touched the patient or table! -1 point!'
+      }
+      function next() {
+        document.querySelector('#warning').textContent = 'Do not touch the patient or table!';
       }
     return (
-        <Container maxWidth="sm" sx={{display: 'flex'}}>
-        <div style={{ background: '#cfe8fc', height: '100vh', width: '50%' }}>
+        <Container maxWidth="lg" sx={{display: 'flex'}}>
+        <div style={{ background: 'white', height: '100vh', width: '50%' }}>
                 {/* <div 
                 style={{height: '100px', width: '100px', background: 'green', marginTop: '70%'}}
                 draggable="true" onDragStart={drag}
@@ -40,11 +50,18 @@ const Operation = ({changeGame}) => {
                 ></div>
          </div>
          <div 
-            style={{background: 'black', height: '100vh', width: '50%'}}
+            style={{background: 'black', height: '100vh', width: '25%'}}
             onDrop={drop}
             onDragOver={allowDrop}
             id="div1"
             ></div>
+        <div 
+            style={{background: 'white', height: '100vh', width: '25%'}}
+        >
+            <h2>Score: <span id='score'>0</span></h2>
+            <p id='warning'>Do not touch the patient or table!</p>
+            <button onClick={next}>Try Again</button>
+        </div>
       </Container>
     )
 }
