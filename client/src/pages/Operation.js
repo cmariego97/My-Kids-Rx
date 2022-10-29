919632fa06f207ab702895067b618f513a777562
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-28 22:35:23 -0400

    added pictures of objects to remove in operation game but need to reposition

--- client/src/pages/Operation.js
+++ client/src/pages/Operation.js
@@ -1,8 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-//import images
+//import game images
 import Man from '../assets/images/operation.jpeg';
+import Pencil from '../assets/images/pencil.png';
+import Butterfly from '../assets/images/butterfly.jpeg';
+import Wishbone from '../assets/images/wishbone.jpeg';
+import Apple from '../assets/images/apple.jpeg';
+import Hammer from '../assets/images/hammer.jpeg';
+import Light from '../assets/images/light.png'
 
 const Operation = ({changeGame}) => {
     let score = 0;
@@ -31,23 +36,25 @@ const Operation = ({changeGame}) => {
         document.querySelector('#warning').textContent = 'Oh no! You touched the patient or table! -1 point!'
       }
       function next() {
-        document.querySelector('#warning').textContent = 'Do not touch the patient or table!';
+        document.querySelector('#warning').textContent = 'Remove the objects that do not belong! Do not touch the patient or table!';
       }
     return (
         <Container maxWidth="lg" sx={{display: 'flex'}}>
         <div style={{ background: 'white', height: '100vh', width: '50%' }}>
-                {/* <div 
-                style={{height: '100px', width: '100px', background: 'green', marginTop: '70%'}}
-                draggable="true" onDragStart={drag}
-                id="drag2"
-                ></div> */}
-                <img src={Man} style={{height: '100vh'}} onClick={penalty}>
+                <img src={Man} alt='operation-game' style={{height: '100vh', border: '1px solid black'}} onClick={penalty}>
                 </img>
-                <div 
-                style={{height: '100px', width: '100px', background: 'red', position: 'relative', bottom: '500px', zIndex: 8}}
-                draggable="true" onDragStart={drag}
-                id="drag1"
-                ></div>
+                <img src={Pencil} alt='pencil' style={{width: '100px', position: 'relative', bottom: '500px'}} draggable="true" onDragStart={drag}
+                id="drag1"></img>
+                <img src={Butterfly} alt='butterfly' style={{width: '50px', position: 'relative', bottom: '300px'}} draggable="true" onDragStart={drag}
+                id="drag2"></img>
+                <img src={Wishbone} alt='wishbone' style={{width: '30px', position: 'relative', bottom: '100px'}} draggable="true" onDragStart={drag}
+                id="drag3"></img>
+                <img src={Apple} alt='apple' style={{width: '30px', position: 'relative', bottom: '100px', right: '200px'}} draggable="true" onDragStart={drag}
+                id="drag4"></img>
+                <img src={Hammer} alt='hammer' style={{width: '30px', position: 'relative', bottom: '300px', right: '200px'}} draggable="true" onDragStart={drag}
+                id="drag5"></img>
+                <img src={Light} alt='light' style={{width: '30px', position: 'relative', bottom: '600px', right: '200px'}} draggable="true" onDragStart={drag}
+                id="drag6"></img>
          </div>
          <div 
             style={{background: 'black', height: '100vh', width: '25%'}}
@@ -59,7 +66,7 @@ const Operation = ({changeGame}) => {
             style={{background: 'white', height: '100vh', width: '25%'}}
         >
             <h2>Score: <span id='score'>0</span></h2>
-            <p id='warning'>Do not touch the patient or table!</p>
+            <p id='warning'>Remove the objects that do not belong! Do not touch the patient or table!</p>
             <button onClick={next}>Try Again</button>
         </div>
       </Container>
