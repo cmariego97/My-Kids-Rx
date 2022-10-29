334c14d4ba25a4184bfbbe061c581daca2f9692e
userrp <109092365+rpecuch@users.noreply.github.com>
2022-10-28 20:49:31 -0400

    added picture of operation game man and played around with overlaying elements on top and dragging and dropping them

--- client/src/pages/Operation.js
+++ client/src/pages/Operation.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+//import images
+import Man from '../assets/images/operation.jpeg';
 
 const Operation = ({changeGame}) => {
     function drag(ev) {
@@ -10,23 +12,31 @@ const Operation = ({changeGame}) => {
       function allowDrop(ev) {
         ev.preventDefault();
       }
+      //TODO: just drag to trash can icon or something
       function drop(ev) {
         ev.preventDefault();
         var data = ev.dataTransfer.getData("text");
         ev.target.appendChild(document.getElementById(data));
+        console.log('dropped');
+      }
+
+      function penalty() {
+        console.log(true);
       }
     return (
         <Container maxWidth="sm" sx={{display: 'flex'}}>
         <div style={{ background: '#cfe8fc', height: '100vh', width: '50%' }}>
-            <div 
-                style={{height: '100px', width: '100px', background: 'red', marginTop: '30%'}}
-                draggable="true" onDragStart={drag}
-                id="drag1"
-                ></div>
-                <div 
+                {/* <div 
                 style={{height: '100px', width: '100px', background: 'green', marginTop: '70%'}}
                 draggable="true" onDragStart={drag}
                 id="drag2"
+                ></div> */}
+                <img src={Man} style={{height: '100vh'}} onClick={penalty}>
+                </img>
+                <div 
+                style={{height: '100px', width: '100px', background: 'red', position: 'relative', bottom: '500px', zIndex: 8}}
+                draggable="true" onDragStart={drag}
+                id="drag1"
                 ></div>
          </div>
          <div 
