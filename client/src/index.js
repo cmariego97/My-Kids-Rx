e7c515a1c97cd5e55a67b0010230cc5ec021fe41
cmariego97 <cmarie.go97@gmail.com>
2022-10-31 11:30:15 -0400

    centralized Theme under index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,65 @@ import App from './App';
 import "@mui/material";
 import "@material-ui/core"
 
+import { createTheme, ThemeProvider } from '@material-ui/core'
+
+// MUI - theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Sunglo
+      main: "#de7171", 
+    },
+    secondary: {
+      // sandrift
+      main: "#a68674", 
+    },
+    error: {
+      // pomegranate
+      main: "#f44336", 
+    },
+    warning: {
+      // carrot-orange
+      main: "#f58c22" 
+    },
+    info: {
+      // fiord
+      main: "#3f4868" 
+    },
+    success: {
+      // asparagus
+      main: "#67a35b" 
+    },
+    neutral: {
+      // gull-gray
+      main: "#9CA6B5"
+    }
+  },
+  typography: {
+    fontFamily: [
+      'Nunito', 'sans-serif', 'Nunito Sans', 'Atma', 'cursive', 'Londrina Solid', 'Josefin Sans'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      color: '#DE7171',
+    },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+    p: {
+      fontSize: 18,
+    }
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+// import CSS
+import '../assets/css/styles.css'
+
 // import MUI styles
-import { createTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
-import {Box, CssBaseline, Typography } from '@material-ui/core';
+import {Box, Typography} from '@mui/material'
+import { CssBaseline } from '@material-ui/core';
 
 //import images
-import background from '../assets/images/site-design-images/plain-animal-bg.svg';
 import AboutUsWhale from '../assets/images/site-design-images/about-us-whale.svg';
 
 //import components
@@ -16,112 +18,25 @@ import Grid from '../components/Grid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStethoscope, faSyringe, faBriefcaseMedical, faMicroscope, faXRay, faFilePrescription } from '@fortawesome/free-solid-svg-icons';
 
-const theme = createTheme({
-    palette: {
-        primary: {
-        // Sunglo
-            main: "#de7171",
-        },
-        secondary: {
-        // sandrift
-            main: "#a68674",
-        },
-        error: {
-        // pomegranate
-            main: "#f44336",
-        },
-        warning: {
-        // carrot-orange
-            main: "#f58c22"
-        },
-        info: {
-        // fiord
-            main: "#3f4868"
-        },
-        success: {
-        // asparagus
-            main: "#67a35b"
-        },
-        neutral: {
-        // gull gray
-            main: "#9CA6B5"
-        }
-    },
-    typography: {
-        fontFamily: [
-            'Nunito', 'sans-serif', 'Nunito Sans', 'Atma', 'cursive', 'Londrina Solid'
-        ],
-        h4: {
-            fontWeight: 600,
-            fontSize: 28,
-            lineHeight: '2rem',
-        },
-        h5: {
-            fontWeight: 100,
-            lineHeight: '2rem',
-        },
-    },
-});
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        minHeight: '100vh',
-        backgroundImage: `url(${background})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-    },
-    wrapper: {
-        width: "65%",
-        margin: "auto",
-        textAlign: "center"
-    },
-    bigSpace: {
-        marginTop: "5rem"
-    },
-    littleSpace: {
-        marginTop: "2.5rem",
-    },
-    grid: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexWrap: "wrap",
-    },
-    aboutUsWrapper: {
-        backgroundColor: "#f4f6fc",
-    },
-    aboutUs: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        margin: "5rem",
-    },
-    textInfo: {
-        color: "#3F4868",
-        textAlign: "justify",
-    },
-}))
-
 export default function Homepage() {
-    const classes = useStyles();
-
     return (
         <Box>
-            <div className={classes.root}>
+            {/* Header */}
+            <Box className="root">
                 <CssBaseline />
                 <Header/>\
-            </div>
+            </Box>
 
             {/* Features and Services */}
-            <div className={classes.wrapper}>
-                <Typography variant="h4" className={classes.bigSpace} color="primary">
+            <Box className="wrapper">
+                <Typography variant="h4" className="bigSpace" color="#DE7171">
                     Our Services + Features
                 </Typography>
-            </div>
+            </Box>
             
             {/* Services Grid */}
             {/* TODO: figure out what to do with the buttons*/}
-            <div className={`${classes.grid} ${classes.bigSpace}`} id="our-services">
+            <Box className="grid bigSpace" id="our-services">
                 {/* <!-- service #1 - physical exams --> */}
                 <Grid icon={<FontAwesomeIcon icon={faStethoscope} style={{color: "#a68674", height: "125", width: "125"}}></FontAwesomeIcon>} title="Appointments" btnTitle="Set Appointment" />
 
@@ -139,22 +54,22 @@ export default function Homepage() {
 
                 {/* <!-- service #6 - prescription --> */}
                 <Grid icon={<FontAwesomeIcon icon={faFilePrescription} style={{color: "#a68674", height: "125", width: "125"}}></FontAwesomeIcon>} title="Medications" btnTitle="Check Orders" />
-            </div>
+            </Box>
 
             {/* About Us */}
-            <div className={classes.aboutUsWrapper}>   
-                <section id="about" className={classes.aboutUs}>
-                    <div className={classes.sectionImg}>
+            <Box className="aboutUsWrapper">   
+                <Box id="about" className="aboutUs">
+                    <Box className="sectionImg">
                         <img src={`${AboutUsWhale}`}/>
-                    </div>
+                    </Box>
                     <Typography variant="h5" color="#3F4868"> 
                         We are a small technology company with a big solution: electronic medical records (EMR) specifically designed for pediatrics! Pediatric patients
                         must be treated differently than adults, as they are still growing and developing. 
                         Why not have the EMR system for their records also be different?! Our staff is composed of four highly motivated full-stack developers 
                         looking to provide your facility with your newest technology upgrade.
                     </Typography>
-                </section>
-            </div>
+                </Box>
+            </Box>
 
                 {/* Footer */}
                 {/* <div className={classes.bigSpace}>
