1c63d7f0cde4adf097e94695529740715a59489e
cmariego97 <cmarie.go97@gmail.com>
2022-11-01 08:58:31 -0400

    cleaned up unused declarations

--- client/src/pages/Appointment.js
+++ client/src/pages/Appointment.js
@@ -1,41 +1,18 @@
-import { ClassNames } from '@emotion/react';
 import React from 'react';
 //import components
 import Appt from '../components/Appt'
 import Note from '../components/Note'
 
 // import MUI styles
-import { createTheme, makeStyles } from '@material-ui/core/styles';
-import { CssBaseline, Typography } from '@material-ui/core';
-//import image
-import background from '../assets/images/site-design-images/plain-animal-bg.svg';
-import { Box } from '@mui/material';
-
-const useStyles = makeStyles((theme) => ({
-    cardAppointments: {
-        display: 'flex',
-        position: 'relative',
-        flexDirection: 'column',
-        flexWrap: 'nowrap',
-        margin: '0.25rem auto',
-        padding: '2rem 2.5rem',
-        width: '55%',
-        minWidth: '550px',
-        minHeight: '500px',
-        backgroundColor: '#F5F2EF',
-        borderRadius: '5px',
-        boxShadow: [ '0 16px 38px -12px rgb(0 0 0 / 56%)', '0 4px 25px 0px rgb(0 0 0 / 12%)', '0 8px 10px -5px rgb(0 0 0 / 20%)' ],
-    }
-}))
+import { Box, Typography } from '@mui/material';
 
 const Appointment = () => {
-const classes = useStyles();
     return(
         <Box className="wrap siteWrap">
-            <Box className={classes.cardAppointments}>
+            <Box className="card">
                 <Typography variant="h5">Upcoming Appointments</Typography>
                 <Appt/>
-                <Typography variant="h5">Past Appointments</Typography>
+                <Typography variant="h5" className="littleSpace">Past Appointments</Typography>
                 <Note/>
             </Box>
         </Box>
--- client/src/pages/Imaging.js
+++ client/src/pages/Imaging.js
@@ -2,94 +2,12 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_IMAGING } from '../utils/queries';
 import Auth from '../utils/auth';
-// import MUI styles
-import { createTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
-import { Box, CssBaseline, Typography } from '@material-ui/core';
-//import image
-import background from '../assets/images/site-design-images/plain-animal-bg.svg';
 
-const theme = createTheme({
-    palette: {
-        primary: {
-        // Sunglo
-            main: "#de7171",
-        },
-        secondary: {
-        // sandrift
-            main: "#a68674",
-        },
-        error: {
-        // pomegranate
-            main: "#f44336",
-        },
-        warning: {
-        // carrot-orange
-            main: "#f58c22"
-        },
-        info: {
-        // fiord
-            main: "#3f4868"
-        },
-        success: {
-        // asparagus
-            main: "#67a35b"
-        },
-        neutral: {
-        // gull gray
-            main: "#9CA6B5"
-        }
-    },
-    typography: {
-        fontFamily: [
-            'Nunito', 'sans-serif', 'Nunito Sans', 'Atma', 'cursive', 'Londrina Solid'
-        ],
-        h1: {
-            fontWeight: 600,
-            fontSize: 80,
-        },
-        h2: {
-            fontWeight: 500,
-            fontSize: 70,
-        },
-        h3: {
-            fontFamily: 'Atma',
-            fontSize: 50,
-        },
-        h4: {
-            fontWeight: 600,
-            fontSize: 28,
-            textTransform: 'uppercase',
-            lineHeight: '2rem',
-        },
-        h5: {
-            fontFamily: 'Nunito',
-            fontWeight: 500,
-            fontSize: 18,
-            textTransform: 'uppercase',
-            lineHeight: '2rem',
-        },
-    },
- });
- 
- const useStyles = makeStyles((theme) => ({
-    cardImaging: {
-        display: 'flex',
-        position: 'relative',
-        flexDirection: 'column',
-        flexWrap: 'nowrap',
-        margin: '0.25rem auto',
-        padding: '2rem 2.5rem',
-        width: '70%',
-        minWidth: '550px',
-        minHeight: '500px',
-        backgroundColor: '#F5F2EF',
-        borderRadius: '5px',
-        boxShadow: [ '0 16px 38px -12px rgb(0 0 0 / 56%)', '0 4px 25px 0px rgb(0 0 0 / 12%)', '0 8px 10px -5px rgb(0 0 0 / 20%)' ],
-    },
- }))
+// import MUI styles
+import { Box, Typography } from '@material-ui/core';
 
 const Imaging= () => {
-    const classes = useStyles();
+
     //retrieve email from jwt to use to find specific patient
     const acctData = Auth.getProfile();
     const email = acctData.data.email;
@@ -136,7 +54,7 @@ const Imaging= () => {
         }
         return (
             <Box className="wrap siteWrap">
-                <Box className={classes.cardImaging}>
+                <Box className="card">
                     <Typography variant="h5">Imaging Results</Typography>
                     {renderImaging()}
                 </Box>
